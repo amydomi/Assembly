cb79cb209e6552b4b7c216e08c142073c7c23b70
Mr.Huang <dusksoft@qq.com>
2019-01-23 17:51:23 +0800

    使用nmake简化编译

--- nmake-demo/main.asm
+++ nmake-demo/main.asm
@@ -14,11 +14,23 @@ include resource.inc
 .const
 szText      db     '是否关闭窗口？', 0
 szTitle     db      '消息', 0
+szBtnText1  db      '你点击了第一个按钮。', 0
+szBtnText2  db      '你点击了第二个按钮。', 0
 
 .data?
 hInstance   dd     ?
 
 .code
+commandProc proc hWnd, uMsg, wParam, lParam
+    mov      eax, wParam
+    movzx    eax, ax
+    .if eax == IDB_BTN1
+        invoke      MessageBox, hWnd, offset szBtnText1, offset szTitle, MB_OK
+    .elseif eax == IDB_BTN2
+        invoke      MessageBox, hWnd, offset szBtnText2, offset szTitle, MB_OK
+    .endif
+    ret
+commandProc endp
 ; 消息过程
 ; >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 dlgProc proc hWnd, uMsg, wParam, lParam
@@ -29,6 +41,8 @@ dlgProc proc hWnd, uMsg, wParam, lParam
             invoke  EndDialog, hWnd, 0
         .endif
         mov     eax, TRUE
+    .elseif eax == WM_COMMAND
+        invoke      commandProc, hWnd, uMsg, wParam, lParam
     .else
         mov     eax, FALSE
         ret
